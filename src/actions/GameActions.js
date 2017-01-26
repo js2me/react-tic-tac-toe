eea037012f12379ec140d707ab4e236ec3640585
Sergey Volkov <truefunrun@hotmail.com>
2017-01-26 18:23:36 +0300

    create new ai

--- src/actions/GameActions.js
+++ src/actions/GameActions.js
@@ -10,7 +10,7 @@ import {
 // import fetch from 'isomorphic-fetch';
 // import {getPlayers,updateWinner} from './GameInfoActions';
 const API_ROOT_URL = 'http://606ep.ru:8080/';
-
+let numNodes = 0;
 let gameField = [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
     gameStatus = 0,
     winLine = [],
@@ -18,7 +18,8 @@ let gameField = [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
         autoPlay: false,
         players: {
             player: 0,
-            enemy: 0
+            enemy: 0,
+            tie: 0
         }
     },
     winners = {
@@ -133,40 +134,22 @@ function checkWin(gField) {
 function AITurn() {
     let players = settings.players;
     let gField = [].concat(gameField);
-    // let aiTurn = '';
-    // let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-    // let inputs = {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0};
-    // for (let i = 0; i < gameData.length; i++) {
-    //     if(aiTurn === '' && (gameData[i][0] == players.player || gameData[i][1] == players.player || gameData[i][2] == players.player)){
-    //         AIHorizontalRow();
-    //     }
-    //     if(aiTurn === '' && (gameData[i][0] == players.player || gameData[i][0] == players.player || gameData[i][0] == players.player)){
-    //
-    //     }
-    //     // var attempts = 0;
-    //     // if(gameData)
-    //     // for (let j = 0; j < gameData[i].length; j++) {
-    //     //     inputs[i+j] = players.enemy == gameData[i][j] ? players.enemy : players.player == gameData[i][j] ? players.player : 0;
-    //     // }
-    // }
-
-
     var tx = null, ty = null, tp = 0; // tp - приоритет выбранной целевой клетки.
     var stX = 0, stY = 0;
     for (stX = 0; stX < gField.length; stX++)
         for (stY = 0; stY < gField[0].length; stY++) // для каждой клетки
         {
             var lC = gField[stX][stY];
+            debugger;
             if ((lC != players.player) && (lC != players.enemy)) { // только для пустых клеток
                 gField[stX][stY] = players.player;
-                if (checkWin(gField) == players.player) { // пробуем победить
+                if (checkWin(gField) === players.player) { // пробуем победить
                     tx = stX;
                     ty = stY;
                     tp = 3;
-                    // alert('ГОНДОН ПОБЕДИЛ');
                 } else if (tp < 3) {
                     gField[stX][stY] = players.enemy;
-                    if (checkWin(gField) == players.enemy) { // или помешать победить игроку.
+                    if (checkWin(gField) === players.enemy) { // или помешать победить игроку.
                         tx = stX;
                         ty = stY;
                         tp = 2;
@@ -177,11 +160,15 @@ function AITurn() {
                         if (stX < 1) mini = 0;
                         if (stY < 1) minj = 0;
 // найти ближайший нолик...
-                        for (var i = mini; i <= maxi; i++) for (var j = minj; j <= maxj; j++) if ((i != 0) && (j != 0)) { // если есть рядом своя занятая клетка - поближе к своим
-                            if (gField[stX + i][stY + j] == players.enemy) {
-                                tx = stX;
-                                ty = stY;
-                                tp = 1;
+                        for (var i = mini; i <= maxi; i++) {
+                            for (var j = minj; j <= maxj; j++) {
+                                if ((i != 0) && (j != 0)) { // если есть рядом своя занятая клетка - поближе к своим
+                                    if (gField[stX + i][stY + j] == players.enemy) {
+                                        tx = stX;
+                                        ty = stY;
+                                        tp = 1;
+                                    }
+                                }
                             }
                         }
                         if (tp < 1) { // или хотя бы на свободную клетку поставить.
@@ -205,6 +192,109 @@ function AITurn() {
 
 
 }
+
+let winnerKeys = {
+    '0':'enemy',
+    '1':'player',
+    '-1':'draw',
+    null:'continue'
+};
+function getWinner(board) {
+
+    // Check if someone won
+    let vals = [settings.players.enemy, settings.players.player];
+    let allNotNull = true;
+    // vals.map((value)=>{
+    //
+    // });
+    for (var k = 0; k < vals.length; k++) {
+        let value = vals[k];
+
+        // Check rows, columns, and diagonals
+        var diagonalComplete1 = true;
+        var diagonalComplete2 = true;
+        for (var i = 0; i < 3; i++) {
+            if (board[i][i] != value) {
+                diagonalComplete1 = false;
+            }
+            if (board[2 - i][i] != value) {
+                diagonalComplete2 = false;
+            }
+            var rowComplete = true;
+            var colComplete = true;
+            for (var j = 0; j < 3; j++) {
+                if (board[i][j] != value) {
+                    rowComplete = false;
+                }
+                if (board[j][i] != value) {
+                    colComplete = false;
+                }
+                if (board[i][j] == null) {
+                    allNotNull = false;
+                }
+            }
+            if (rowComplete || colComplete) {
+                return value;
+            }
+        }
+        if (diagonalComplete1 || diagonalComplete2) {
+            return value;
+        }
+    }
+    //tie!
+    if (allNotNull) {
+        return -1;
+    }
+    //winner not found
+    return null;
+}
+
+
+function recurseMinimax(board, enemy, player) {
+    numNodes++;
+    var winner = getWinner(board);
+    if (winner != null) {
+        // winnerFinded()
+        switch(winner) {
+            case 1:
+                // AI wins
+                return [1, board];
+            case 0:
+                // opponent wins
+                return [-1, board];
+            case -1:
+                // Tie
+                return [0, board];
+        }
+    } else {
+        // Next states
+        var nextVal = null;
+        var nextBoard = null;
+
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 3; j++) {
+                if (board[i][j] == null) {
+                    board[i][j] = enemy;
+                    var value = recurseMinimax(board, enemy, player)[0];
+                    if ((enemy && (nextVal == null || value > nextVal)) || (player && (nextVal == null || value < nextVal))) {
+                        nextBoard = board.map(arr => arr.slice());
+                        nextVal = value;
+                    }
+                    board[i][j] = null;
+                }
+            }
+        }
+        return [nextVal, nextBoard];
+    }
+}
+
+
+function AITurn2(gameField){
+    numNodes = 0;
+    return recurseMinimax(gameField, settings.players.enemy, settings.players.player)[1];
+}
+
+
 function winnerFinded(dispatch, winner) {
     gameStatus = winner;
     if (gameStatus !== 'D') {
@@ -214,15 +304,18 @@ function winnerFinded(dispatch, winner) {
                 whoWinner = index;
             }
         });
+        // Object.keys(settings.players).map(index=>
+        //     settings.players[index] === winner ?
+        //         whoWinner = index :
+        //         whoWinner = '');
         if (!gameover) {
             winners[whoWinner]++;
             gameover = true;
         }
-    } else winLine = [];
-    // if(settings.autoPlay){
-    //     console.log('ЗАШЛО :');
-    //     setTimeout(getGameFieldData,2000);
-    // }
+    }
+    else {
+        winLine = [];
+    }
     dispatch({
         type: UPDATE_GAME_STATUS,
         gameStatus, winLine, winners
@@ -242,9 +335,9 @@ export function getGameFieldData() {
             winLine, winners, gameStatus
         });
         let randGameFieldData = Math.floor(Math.random() * 19);
-        if(randGameFieldData > 10){
+        if (randGameFieldData > 10) {
             loadGameFieldData(dispatch);
-        }else{
+        } else {
             gameField = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
             dispatch({
                 type: GET_GAME_DATA,
@@ -254,9 +347,9 @@ export function getGameFieldData() {
     }
 
 }
-export function resetGameStatus(){
+export function resetGameStatus() {
     gameStatus = 0;
-    return (dispatch) =>{
+    return (dispatch) => {
         dispatch({
             type: RESET_GAME_STATUS,
             gameStatus
@@ -264,25 +357,24 @@ export function resetGameStatus(){
     }
 }
 export function makeGameTurn(row, cell, value) {
-        return (dispatch) => {
-
-            if (!gameover) {
-                var victory = checkWin(gameField); // проверка на победу.
-                if (!victory) {
-                    gameField[row][cell] = value;
-                    AITurn();
-                    victory = checkWin(gameField); // проверка на победу
-                    dispatch({
-                        type: UPDATE_GAME_DATA,
-                        payload: gameField
-                    });
-                }
-                if (victory === 'D' || victory) {
-                    winnerFinded(dispatch, victory);
-                }
+    return (dispatch) => {
+        if (!gameover) {
+            var victory = checkWin(gameField); // проверка на победу.
+            if (!victory) {
+                gameField[row][cell] = value;
+                AITurn();
+                victory = checkWin(gameField); // проверка на победу
+                dispatch({
+                    type: UPDATE_GAME_DATA,
+                    payload: gameField
+                });
             }
-
+            if (victory === 'D' || victory) {
+                winnerFinded(dispatch, victory);
+            }
         }
+
+    }
 }
 export function updatePlayersData(playerChoice) {
     return (dispatch) => {
